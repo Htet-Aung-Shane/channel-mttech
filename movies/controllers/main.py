46d2a68f0f61ed5c6211326fc0917cfaaf927fdd
has <has@pop-os.localdomain>
2023-09-08 23:47:31 +0630

    insert category

--- movies/controllers/main.py
+++ movies/controllers/main.py
@@ -8,6 +8,15 @@ class BookShow(http.Controller):
     @http.route(['/movies'], type='http',
                 auth='public', website=True)
     def show(self, **get):
+        country_ids = request.env['res.country'].sudo().search([])
+        genre_ids = request.env['mt.genre'].sudo().search([])
+        category_ids = request.env['mt.category'].sudo().search([])
+        type_ids = request.env['mt.tag'].sudo().search([])
+        cast_ids = request.env['mt.cast'].sudo().search([])
+        print('countries>',country_ids,'genre',genre_ids,'category',category_ids,'type_ids',type_ids,'cast_ids',cast_ids)
+        get.update({
+                'countries': country_ids,
+            })
         return http.request.render(
             "movies.movies_list",get
         )
